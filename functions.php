05a12f7c8f9c8e003620e35a0c2530053174629d
influxion <nicholsjordon721@gmail.com>
2022-09-15 23:00:13 -0500

    Added Blog section, fixed styling, homepage work, etc..

--- functions.php
+++ functions.php
@@ -8,12 +8,13 @@ function university_files()
     wp_enqueue_style('university_main_styles', get_theme_file_uri('./build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('./build/index.css'));
 }
-
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
+    // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    // register_nav_menu('footerLocationOne', 'Footer Location One');
+    // register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
-
 add_action('after_setup_theme', 'university_features');
